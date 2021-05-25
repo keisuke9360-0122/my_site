825315c3bcc8305a0b9a1dd8f8b563e2d4924bf0
keisuke9360-0122 <gz5y85tm3artsy4@i.softbank.jp>
2021-05-25 22:55:54 +0900

    "Web work"セクション実装終了

--- mysite.js
+++ mysite.js
@@ -1,6 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   $(".wrap").slideUp(2000);
 });
-(function (){
 
+$('#page-link a[href*="#"]').click(function () {
+  let elmHash = $(this).attr("href"); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+  let pos = $(elmHash).offset().top; //idの上部の距離を取得
+  $("body,html").animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+  return false;
+});
+
+$(".next").click(function () {
+  $(".slide-list:not(:animated)").animate(
+    {
+      "margin-left": -1 * $(".slide-item").width(),
+    },
+    function () {
+      $(".slide-list").append($(".slide-item:first-child"));
+      $(".slide-list").css("margin-left", "0");
+    }
+  );
+});
+$(".prev").click(function () {
+  $(".slide-list:not(:animated)")
+    .prepend($(".slide-item:last-child"))
+    .css("margin-left", -1 * $(".slide-item").width())
+    .animate({
+      "margin-left": 0,
+    });
 });
