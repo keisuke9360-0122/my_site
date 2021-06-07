97ce1517511bc4d97ed1914bd6a5d35b3d76fdd7
keisuke9360-0122 <gz5y85tm3artsy4@i.softbank.jp>
2021-06-07 20:16:25 +0900

    ファーストビュー画像変更、スクロールイベント追加

--- mysite.js
+++ mysite.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   $(".wrap").slideUp(2000);
 });
-$(function(){
-  $(window).scroll(function (){
-      $('.fadein').each(function(){
-          const position = $(this).offset().top;
-          const scroll = $(window).scrollTop();
-          const windowHeight = $(window).height();
-          if (scroll > position - windowHeight + 200){
-            $(this).addClass('active');
-          }
-      });
+$(function () {
+  $(window).scroll(function () {
+    $(".fadein").each(function () {
+      const position = $(this).offset().top;
+      const scroll = $(window).scrollTop();
+      const windowHeight = $(window).height();
+      if (scroll > position - windowHeight + 200) {
+        $(this).addClass("active");
+      }
+    });
   });
+  
 });
 
 $('#page-link a[href*="#"]').click(function () {
@@ -21,26 +22,26 @@ $('#page-link a[href*="#"]').click(function () {
   return false;
 });
 
-  $(window).on('load',function(){
-  $("#splash-logo").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
-  
+
+$(window).on("load", function () {
+  $("#splash-logo").delay(1200).fadeOut("slow"); //ロゴを1.2秒でフェードアウトする記述
+
   //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-  $("#splash").delay(1500).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
-  
-  $("section").addClass('appear');//フェードアウト後bodyにappearクラス付与
-  
-  });
+  $("#splash")
+    .delay(1500)
+    .fadeOut("slow", function () {
+      //ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+
+      $("section").addClass("appear"); //フェードアウト後bodyにappearクラス付与
+    });
   //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-  
+
   //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
-  $('.splashbg').on('animationend', function() { 
-  //この中に動かしたいJSを記載
+  $(".splashbg").on("animationend", function () {
+    //この中に動かしたいJSを記載
   });
   //=====ここまで背景が伸びた後に動かしたいJSをまとめる
-  
-  });
-
-
+});
 
 $(".next").click(function () {
   $(".slide-list:not(:animated)").animate(
@@ -92,37 +93,55 @@ const swiper = new Swiper(".main-image02-thumbs", {
   $(".main-image02-thumbs__item").on("click", function () {
     $("li").classList.remove("swiper-slide-thumb-active");
   });
-
-
 });
 
 $(".image").modaal({
   type: "image",
 });
 $(".gallery-list").modaal({
-  fullscreen:'true', //フルスクリーンモードにする
-  before_open:function(){// モーダルが開く前に行う動作
-    $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+  fullscreen: "true", //フルスクリーンモードにする
+  before_open: function () {
+    // モーダルが開く前に行う動作
+    $("html").css("overflow-y", "hidden"); /*縦スクロールバーを出さない*/
   },
-  after_close:function(){// モーダルが閉じた後に行う動作
-    $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
-  }
-});
-function fadeAnime() {
-  //ふわっと動くきっかけのクラス名と動きのクラス名の設定
-  $(".category-text").each(function () {
-    //fadeInUpTriggerというクラス名が
-    const elemPos = $(this).offset().top - 10; //要素より、50px上の
-    const scroll = $(window).scrollTop();
-    const windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight) {
-      $(this).addClass("fadeUp"); // 画面内に入ったらfadeInというクラス名を追記
-    } else {
-      $(this).removeClass("fadeUp"); // 画面外に出たらfadeInというクラス名を外す
-    }
-  });
-}
-$(window).scroll(function () {
-  fadeAnime(); /* アニメーション用の関数を呼ぶ*/
+  after_close: function () {
+    // モーダルが閉じた後に行う動作
+    $("html").css("overflow-y", "scroll"); /*縦スクロールバーを出す*/
+  },
 });
 
+function moveAnimation() {
+  let randomElm2 = $(".randomScroll"); //親要素取得
+  let randomElm2Child = $(randomElm2).children(); //親の子要素を取得
+  randomScrollAnime();
+  function randomScrollAnime() {
+    let elemPos = $(".randomScroll").offset().top - 50; //要素より、50px上まで来たら
+    let scroll = $(window).scrollTop();
+    let windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      if (randomElm2Child.length > 0) {
+        //配列数以上であれば処理をおこなう
+        let rnd = Math.floor(Math.random() * randomElm2Child.length); //配列数から表示する数値をランダムで取得
+        let moveData = "fadeUphair"; //アニメーション名＝CSSのクラス名を指定
+        if (animeFlag) {
+          //スクロールする度に動作するのでアニメーションが終わるまで処理をさせないようにする
+          animeFlag = false; //アニメーション処理が終わるまで一時的にfalseにする
+          $(randomElm2Child[rnd]).addClass(moveData); //アニメーションのクラスを追加
+          setTimeout(function () {
+            animeFlag = true; //次の処理をおこなうためにtrueに変更
+            randomScrollAnime(); //自身の処理を繰り返す
+          }, 500); //0.5秒間隔で。※ランダムのスピード調整はこの数字を変更させる
+          randomElm2Child.splice(rnd, 1); //アニメーション追加となった要素を配列から削除
+        }
+      }
+    } else {
+      animeFlag = true;
+    }
+  }
+}
+let animeFlag = true; //スクロールする度に動作するのでアニメーションが終わるまで処理をさせないようにするための定義
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  moveAnimation(); /* アニメーション用の関数を呼ぶ*/
+});
