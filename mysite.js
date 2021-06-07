6a5ee73fae2d4dc925d490404c278e0dc9a1f38f
keisuke9360-0122 <gz5y85tm3artsy4@i.softbank.jp>
2021-06-07 13:08:22 +0900

    footer仮実装

--- mysite.js
+++ mysite.js
@@ -1,6 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   $(".wrap").slideUp(2000);
 });
+$(function(){
+  $(window).scroll(function (){
+      $('.fadein').each(function(){
+          const position = $(this).offset().top;
+          const scroll = $(window).scrollTop();
+          const windowHeight = $(window).height();
+          if (scroll > position - windowHeight + 200){
+            $(this).addClass('active');
+          }
+      });
+  });
+});
 
 $('#page-link a[href*="#"]').click(function () {
   let elmHash = $(this).attr("href"); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
@@ -8,6 +20,27 @@ $('#page-link a[href*="#"]').click(function () {
   $("body,html").animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
   return false;
 });
+
+  $(window).on('load',function(){
+  $("#splash-logo").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
+  
+  //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+  $("#splash").delay(1500).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+  
+  $("section").addClass('appear');//フェードアウト後bodyにappearクラス付与
+  
+  });
+  //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+  
+  //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
+  $('.splashbg').on('animationend', function() { 
+  //この中に動かしたいJSを記載
+  });
+  //=====ここまで背景が伸びた後に動かしたいJSをまとめる
+  
+  });
+
+
 
 $(".next").click(function () {
   $(".slide-list:not(:animated)").animate(
